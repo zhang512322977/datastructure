22a0bc6668d1ad8acf286558a9435408da149325
zl_zhangdong <zl_zhangdong@hzzl.com>
2019-09-25 19:00:45 +0800

    ces

--- src/main/java/org/buptdavid/datastructure/excel/ExcelWriteTest.java
+++ src/main/java/org/buptdavid/datastructure/excel/ExcelWriteTest.java
@@ -8,6 +8,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import org.buptdavid.datastructure.http.GetServiceDSF;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -113,6 +114,11 @@ public class ExcelWriteTest {
     public static void main(String[] args)
         throws IOException
     {
+
+        File file = new File("");
+
+        FileOutputStream fileOutputStream = new  FileOutputStream(file);
+
 
         writeWithoutHead();
     }
